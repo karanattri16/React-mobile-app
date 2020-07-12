850c67cfc425df7e0a9ac3214573223375c5815e
Karan <karanattri6760@gmail.com>
2020-07-12 09:21:23 +0530

    cart count badge added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,12 @@ import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 import styled from "styled-components";
 import { ButtonContainer } from "./Button";
-
+import {ProductComsumer} from '../Context'
 class Navbar extends Component {
   render() {
     return (
       <div>
         <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-          {/* 
-https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */}
           <Link to="/">
             <img src={logo} alt="store" className="navbar-brand" />
           </Link>
@@ -23,13 +19,22 @@ https://www.iconfinder.com/Makoto_msk */}
               </Link>
             </li>
           </ul>
-          <Link to="/cart" className="ml-auto">
-            <ButtonContainer>
-              <span className="mr-2">
-                <i className="fas fa-cart-plus">my cart</i>
-              </span>
-            </ButtonContainer>
-          </Link>
+          <ProductComsumer>
+            {value=>{
+              const cart = value.cart;
+              return(
+                <Link to="/cart" className="ml-auto">
+                  <ButtonContainer>
+                    <span className="mr-2">
+                      <i className="fas fa-cart-plus">my cart 
+                      <span className="badge badge-light">{cart.length}</span>
+                      </i>
+                    </span>
+                  </ButtonContainer>
+                </Link>
+              )
+            }}
+          </ProductComsumer>          
         </NavWrapper>
       </div>
     );
